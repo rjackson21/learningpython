bcc43d4b30c8c3d4db1ad01ff5bce7cd553f0c92
Regina Jackson <rjackson156@gmail.com>
2024-10-19 21:36:18 -0400

    Learn Python Exercise 5

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-import pandas
+my_age = 27
+my_height = 69
+my_weight = 145
 
-print()
-print("Hello World!")
-print()
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight}, I get {total}.")
